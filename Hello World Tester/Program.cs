91f97dca5669593afed0c24ba7bfe91f7d646b04
FrameworkTV <ioanejaniashvili19@gmail.com>
2023-11-21 10:52:33 +0400

    Add project files.

---
+++ Hello World Tester/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Hello_World_Tester
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
